2390e49071bdf625a2e92f4d86a7b2f645001dc6
Artur_Mukhametdinov <ekb-led2012@yandex.ru>
2024-10-15 10:31:54 +0500

    deploy.yml

---
+++ lab4/src/task1/Main.java
@@ -0,0 +1,26 @@
+package task1;
+
+public class Main {
+    public static void main(String[] args) {
+//        Numbers.sample1();
+//        System.out.println();
+//
+//        Numbers.sample2();
+//        System.out.println();
+//
+//        Numbers.getVal(-1);
+//        System.out.println();
+// ------------------------------
+//        Dates.getDates();
+//        System.out.println();
+// ------------------------------
+//        Decimals.sample1();
+//        System.out.println();
+//
+//        Decimals.sample2();
+//        System.out.println();
+// ------------------------------
+
+//        Print.sample1();
+    }
+}
